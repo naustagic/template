4cc41f39b30989fa0a3a43ab98eb88ca9444e1b0
Russell Purinton <russell.purinton@gmail.com>
2022-09-24 20:54:11 +0000

    first

---
+++ src/InboxHandler.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace RPurinton\Template;
+
+require_once(__DIR__."/SqlClient.php");
+require_once(__DIR__."/BunnyClient.php");
+require_once(__DIR__."/BunnyAsyncClient.php");
+
+class InboxHandler Extends ConfigLoader
+{
+	private $sql = null;
+	private $bunny = null;
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->sql = new SqlClient();
+		$loop = \React\EventLoop\Factory::create();
+		$this->bunny = new BunnyAsyncClient($loop,"template_inbox",$this->process(...));
+		$loop->run();
+        }
+
+	private function process($message)
+	{
+		switch($message["t"])
+		{
+			case "MESSAGE_CREATE": return $this->MESSAGE_CREATE($message["d"]);
+			case "MESSAGE_UPDATE": return $this->MESSAGE_UPDATE($message["d"]);
+			case "MESSAGE_DELETE": return $this->MESSAGE_DELETE($message["d"]);
+		}
+		return true;
+	}
+
+	private function MESSAGE_CREATE($message)
+	{
+		return true;
+	}
+
+	private function MESSAGE_UPDATE($message)
+	{
+		return true;
+	}
+
+	private function MESSAGE_DELETE($message)
+	{
+		return true;
+	}
+
+	private function reply($message,$reply)
+	{
+		$reply["command-reply"] = true;
+		$reply["function"] = "MESSAGE_REPLY";
+		$reply["reply_to"] = $message["id"];
+		$reply["channel_id"] = $message["channel_id"];
+		$this->bunny->publish("template_outbox",$reply);
+		return true;
+	}
+
+}
---
+++ src/testInboxHandler.php
@@ -0,0 +1,4 @@
+#!/usr/local/bin/php -f
+<?php
+require_once("InboxHandler.php");
+new \RPurinton\Template\InboxHandler;
